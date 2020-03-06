197528a7f36252177ba062af5d4e80c8fe1c535a
LEE HYUN WOO <lhwda1388@naver.com>
2020-03-06 23:12:21 +0900

    pubkey setting

--- src/main/java/com/sample/rsserver/config/ResourceServerConfig.java
+++ src/main/java/com/sample/rsserver/config/ResourceServerConfig.java
@@ -1,13 +1,20 @@
 package com.sample.rsserver.config;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -32,10 +39,34 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		return new JwtTokenStore(accessTokenConverter());
 	}
 	
+	/*
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 	    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 	    converter.setSigningKey(signKey);
 	    return converter;
+	}
+	*/
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter convter = new JwtAccessTokenConverter();
+		Resource resource = new ClassPathResource("pub.txt");
+		String pubKey = null;
+		try {
+			BufferedInputStream bis =  new BufferedInputStream(resource.getInputStream());
+			ByteArrayOutputStream buf = new ByteArrayOutputStream();
+			int result;
+			while((result = bis.read()) != -1) {
+				buf.write((byte) result);
+			}
+			pubKey = buf.toString(Charset.defaultCharset());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		convter.setVerifierKey(pubKey);
+		
+		return convter;
 	}
 }
